6b3c74c93dece688422f9f30f3e5b32dd9d4f1b9
Daniel2006B <Danylo.Brudnyi.RI.2023@lpnu.ua>
2024-04-24 14:12:49 +0300

    Categories with products from server.js мій код

--- server.js
+++ server.js
@@ -2,53 +2,30 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
-// Змінні для зберігання списків категорій і товарів (тимчасове рішення без бази даних)
+// Масив категорій (тимчасове рішення без бази даних)
 let categories = [
-    { id: 1, name: 'одяг' },
-    { id: 2, name: 'взуття' },
-    { id: 3, name: 'аксесуари' }
+    { id: 1, name: 'Одяг', description: 'Вміст категорії Одяг' },
+    { id: 2, name: 'Взуття', description: 'Вміст категорії Взуття' },
+    { id: 3, name: 'Аксесуари', description: 'Вміст категорії Аксесуари' }
 ];
-
+// Масив продуктів (тимчасове рішення без бази даних)
 let products = [];
-
-// Додавання продукту до масиву products
-products.push({
-    id: 1,
-    name: 'Сукня',
-    price: 100,
-    category: { id: 1, name: 'одяг' }
-});
-
-
-products.push({
-    id: 2,
-    name: 'Мокасіни',
-    price: 100,
-    category: { id: 2, name: 'взуття' }
-})
-
-app.use(express.json());
-
 // Роут для отримання списку категорій
 app.get('/categories', (req, res) => {
     res.json(categories);
 });
 
-// Роут для отримання списку товарів
-app.get('/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/add-product', (req, res) => {
-    const { name, categoryId } = req.query;
+// Роут для створення нового продукту
+app.post('/add-product', (req, res) => {
+    const { name, categoryId } = req.body;
     const category = categories.find(cat => cat.id === parseInt(categoryId));
     if (!category) {
-        return res.status(400).json({ message: 'Категорія не знайдена'});
+        return res.status(400).json({ message: 'Категорія не знайдена' });
     }
     const newProduct = { id: products.length + 1, name, categoryId };
     products.push(newProduct);
@@ -59,5 +36,5 @@ const port = process.env.PORT || 1337;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log(`The server is started on the port ${port}`);
+    console.log(`Сервер запущено на порту ${port}`);
 });
